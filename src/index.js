712d2f375f8097fb5bdcd55973b3a47a9d5fb6a1
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2023-02-06 22:47:11 +0000

    req 4

--- src/index.js
+++ src/index.js
@@ -30,6 +30,43 @@ app.get('/talker/:id', async (req, res) => {
    return res.status(HTTP_OK_STATUS).json(talk);
 });
 
-app.post('/login', (_req, res) => {
-    res.status(HTTP_OK_STATUS).json({ token: talker.randomToken(16) });
-});
+const validateLogin = (req, res, next) => {
+  const requiredProperties = ['email', 'password'];
+  if (requiredProperties.every((property) => property in req.body)) {
+    next();
+  } if (!req.body.email) {
+   return res.status(400).json({
+      message: 'O campo "email" é obrigatório',
+    });
+  }
+  if (!req.body.password) {
+    return res.status(400).json({
+       message: 'O campo "password" é obrigatório',
+     });
+   }
+};
+
+const validateEmailType = (req, res, next) => {
+  const { email } = req.body;
+    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    next();
+  } else {
+    res.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+};
+
+const validatePasswordType = (req, res, next) => {
+  const { password } = req.body;
+    if (password.length >= 6) {
+    next();
+  } else {
+    return res.status(400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    }); 
+  }
+};
+
+app.post('/login', validateLogin, validateEmailType, validatePasswordType,
+ (_req, res) => res.status(HTTP_OK_STATUS).json({ token: talker.randomToken(16) }));
